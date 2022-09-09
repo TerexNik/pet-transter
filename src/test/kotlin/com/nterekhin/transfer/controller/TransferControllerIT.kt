cc38e9b347b480ef679e2c5da2b9640ad3b67820
nterehin <nterehin@mobiledimension.ru>
2022-09-09 22:34:16 +0300

    0.1: make more accurate harder test

--- src/test/kotlin/com/nterekhin/transfer/controller/TransferControllerIT.kt
+++ src/test/kotlin/com/nterekhin/transfer/controller/TransferControllerIT.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.util.ResourceUtils
 
 @AutoConfigureWebTestClient
 class TransferControllerIT : FullContextTest() {
@@ -14,7 +15,11 @@ class TransferControllerIT : FullContextTest() {
 
     @Test
     fun shouldGetAccountDetails() {
+        val validAccountJson = ResourceUtils.getFile("classpath:json/account.json")
+
         webTestClient.get().uri("/account").exchange()
             .expectStatus().isOk
+            .expectBody()
+            .json(validAccountJson.readText())
     }
 }
