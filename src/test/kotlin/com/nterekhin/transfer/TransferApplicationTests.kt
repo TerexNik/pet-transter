76d73321515e528b33c1ab28481be3c2113f934a
nterehin <nterehin@mobiledimension.ru>
2022-09-09 21:28:37 +0300

    Init commit

---
+++ src/test/kotlin/com/nterekhin/transfer/TransferApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.nterekhin.transfer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TransferApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
